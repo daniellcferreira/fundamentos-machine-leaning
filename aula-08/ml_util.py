de5aca1765953eaf94c03e3812cb13c780e49882
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-04-26 15:32:50 -0300

    update lesson aula08

--- aula-08/ml_util.py
+++ aula-08/ml_util.py
@@ -12,8 +12,8 @@ def regression_metrics(model, X, y, label="teste", plt=False, distro_residues=Fa
 
     print(f"\nMetricas de avaliação (dados de {label}):\n")
 
-    if plot:
-        plot_scatter_real(y, y_pred)
+    if plt:
+        plt.scatter.real.pred(y, y_pred)
 
     r2 = r2_score(y, y_pred)
     mae = mean_absolute_error(y, y_pred)
@@ -30,7 +30,7 @@ def regression_metrics(model, X, y, label="teste", plt=False, distro_residues=Fa
         print(f"\nDistribuição de residuos de {label}:\n")
         print(residuos.describe())
 
-    if plot:
+    if plt:
         sns.histplot(residuos, kde=True)
         plt.show()
 
@@ -38,12 +38,12 @@ def regression_metrics(model, X, y, label="teste", plt=False, distro_residues=Fa
 
 def reg_metrics_train_test(
     model, X_train, y_train, X_test, y_test,
-    calc_metrics_train=True, plot=False, distro_residues=False
+    calc_metrics_train=True, plt=False, distro_residues=False
 ):
-    if calc_metrics_train_test:
-        metrics_train = regrisson_metrics( 
+    if calc_metrics_train:
+        metrics_train = regression_metrics( 
             model, X_train, y_train,
-            label="treino", plot=plot, distro_residues=distro_residues
+            label="treino", plt=plt, distro_residues=distro_residues
         )
 
         print("------------------------------------")
@@ -53,7 +53,7 @@ def reg_metrics_train_test(
 
     metrics_test = regression_metrics(
         model, X_test, y_test,
-        label="teste", plot=plot, distro_residues=distro_residues
+        label="teste", plt=plt, distro_residues=distro_residues
     )
 
     return metrics_train, metrics_test
